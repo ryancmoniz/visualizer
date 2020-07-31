6f2a4cb733c3df0c153736cc3977ab6eb79ab48e
ryancmoniz <ryancmoniz@gmail.com>
2020-07-30 23:23:33 -0400

    add bubbleSort helper

--- client/helpers/bubbleSort.js
+++ client/helpers/bubbleSort.js
@@ -1,15 +1,17 @@
-const bubble = (inputArr) => {
+const bubble = (inputArr, animations = []) => {
+  const newArray = inputArr;
   const len = inputArr.length;
   for (let i = 0; i < len; i += 1) {
     for (let j = 0; j < len; j += 1) {
-      if (inputArr[j] > inputArr[j + 1]) {
-        const tmp = inputArr[j];
-        inputArr[j] = inputArr[j + 1];
-        inputArr[j + 1] = tmp;
+      if (newArray[j] > newArray[j + 1]) {
+        const tmp = newArray[j];
+        newArray[j] = newArray[j + 1];
+        newArray[j + 1] = tmp;
+        animations.push([j, j + 1]);
       }
     }
   }
-  return inputArr;
+  return animations;
 };
 
 export default bubble;
